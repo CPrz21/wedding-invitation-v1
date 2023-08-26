7f6481169eaf58565b5aaf33c7045dce6043ec56
CPrz21 <faselona@gmail.com>
2023-08-26 11:44:35 -0600

    feat: adds wedding info section

---
+++ components/wedding-info-section/event-card/index.tsx
@@ -0,0 +1,50 @@
+import { AiOutlineRight } from 'react-icons/ai';
+import { FaLocationDot } from 'react-icons/fa6';
+import { FiClock } from 'react-icons/fi';
+
+interface EventCardProps {
+  backgroundImageUrl: string;
+  title: string;
+  description: string;
+  locationText: string;
+  eventHourText: string;
+  locationUrl: string;
+}
+
+export const EventCard = ({
+  backgroundImageUrl,
+  title,
+  description,
+  locationText,
+  locationUrl,
+  eventHourText,
+}: EventCardProps) => {
+  return (
+    <div className='w-full flex flex-col md:flex-row my-8'>
+      <div
+        className={`w-full md:w-1/3 h-64 md:h-auto`}
+        style={{
+          background: `url(${backgroundImageUrl}) center center/cover no-repeat local`,
+        }}
+      />
+      <div className='w-full md:w-2/3 px-8 order-3 py-8 md:py-0'>
+        <h3 className='text-dark-color mb-4'>{title}</h3>
+        <p className='flex items-center text-gray-color gap-1 mb-2'>
+          <FaLocationDot color='#5e9a8e' /> {locationText}
+        </p>
+        <p className='flex items-center text-gray-color gap-1'>
+          <FiClock color='#5e9a8e' /> {eventHourText}
+        </p>
+        <p className='text-gray-color my-4'>{description}</p>
+        <a
+          target='_blank'
+          href={locationUrl}
+          className='text-primary-color flex items-center gap-1'
+          rel='noreferrer'>
+          <strong>See Location</strong>
+          <AiOutlineRight />
+        </a>
+      </div>
+    </div>
+  );
+};
--- components/wedding-info-section/index.tsx
+++ components/wedding-info-section/index.tsx
@@ -1,9 +1,27 @@
 import { SectionTitle } from '@components/shared';
 
+import { EventCard } from './event-card';
+
 export const WeddingInfoSection = () => {
   return (
     <section className='container mx-auto section-container'>
       <SectionTitle text='Wedding Events' />
+      <EventCard
+        backgroundImageUrl='/images/reception-image.jpeg'
+        title='THE RECEPTION'
+        description='when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+        locationText='32 big bro road, Chanpai, London.'
+        eventHourText='Nov 25 2017, 9AM - 5PM'
+        locationUrl='https://goo.gl/maps/BoWj1wzxgHs12hBw8'
+      />
+      <EventCard
+        backgroundImageUrl='/images/wedding-party-image.jpeg'
+        title='WEDDING PARTY'
+        description='when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+        locationText='32 big bro road, Chanpai, London.'
+        eventHourText='Nov 25 2017, 9AM - 5PM'
+        locationUrl='https://goo.gl/maps/BoWj1wzxgHs12hBw8'
+      />
     </section>
   );
 };
