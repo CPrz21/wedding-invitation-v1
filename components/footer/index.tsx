6cb803c3b197136fa115fa666821b44bbf3a3b51
CPrz21 <faselona@gmail.com>
2023-09-03 18:17:26 -0600

    fix: removes texts from footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,15 +1,13 @@
-import { great_vibes } from '@app/fonts';
-
 import Styles from './footer.styles.module.scss';
 
 export const Footer = () => {
   return (
-    <footer className={`${Styles['footer-container']} py-28 relative`}>
-      <h2
+    <footer className={`${Styles['footer-container']} py-36 relative`}>
+      {/* <h2
         className={`${great_vibes.className} text-center text-white relative z-20 text-4xl md:text-6xl`}>
         Por Siempre y Para Siempre <br />
         Nuestro Cariño
-      </h2>
+      </h2> */}
     </footer>
   );
 };
